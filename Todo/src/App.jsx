c61c0e2c49b2b41696676cad385556318ff21a40
Dom-iha <taqib.ibrahim@gmail.com>
2023-02-27 23:55:43 +0100

    setup React App

--- Todo/src/App.jsx
+++ Todo/src/App.jsx
@@ -7,11 +7,11 @@ import Main from './components/Footer'
 function App() {
   
    return (
-      <div className="App">
+      <>
          <Header />
          <Main />
          <Footer />
-      </div>
+      </>
    )
 }
 
