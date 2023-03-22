aac55d8bd363c2c35c2623e0009f299e064a8d26
Dom-iha <taqib.ibrahim@gmail.com>
2023-03-23 00:53:16 +0100

    initialize dir

---
+++ expense-app/src/components/NewExpense/NewExpense.jsx
@@ -0,0 +1,21 @@
+import ExpenseForm from "./ExpenseForm";
+
+const NewExpense = (props) => {
+    const onSaveExpenseDataHandler = (enteredExpenseData) => {
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.floor(Math.random() * 100).toString()
+        };
+        props.onAddExpense(expenseData)
+    };
+
+    return (
+        <div className="new-expense">
+           <ExpenseForm 
+                onSaveExpenseData={onSaveExpenseDataHandler}
+           /> 
+        </div>
+    )
+}
+
+export default NewExpense;
